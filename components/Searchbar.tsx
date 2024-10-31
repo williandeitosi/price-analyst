b592bec7f78cd70b93dfcbef4c8d27347af6cbda
williandeitosi <williandeitosi@gmail.com>
2024-10-30 22:42:44 -0300

    feat: lib actions

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,11 +1,61 @@
-'use client'
+"use client";
+
+import { useState, type FormEvent } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 
 const Searchbar = () => {
-  const handleSubmit = () => { }
-  return <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-    <input type="text" placeholder="Entrer product link" className="searchbar-input" />
-    <button type="submit" className="searchbar-btn">Search</button>
-  </form>;
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid amazon link");
+
+    try {
+      setIsLoading(true);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Entrer product link"
+        className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Search..." : "Search"}
+      </button>
+    </form>
+  );
 };
 
 export default Searchbar;
---
+++ lib/scraper/index.ts
@@ -0,0 +1,19 @@
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+
+  // curl -i --proxy brd.superproxy.io:22225 --proxy-user
+  // brd-customer-hl_ffc6b9f3-zone-priceanalyst:uy88rifnm4pa -k "https://
+  // geo.brdtest.com/welcome.txt"
+
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+  };
+}
