fce96ab374a6c777860c81f028659ca5d4eee87d
williandeitosi <williandeitosi@gmail.com>
2024-11-03 23:22:02 -0300

    feat: get all information to product

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -24,8 +24,50 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
-    const title = $("#productTitle").text().trim();
-    console.log({ title });
+    const title = $("#productTitle").first().text().trim();
+    const currentPrice = $("span.a-price-whole")
+      .first()
+      .text()
+      .trim()
+      .replace(",", "");
+    const priceAsnumber = $(".a-price.a-text-price span.a-offscreen")
+      .first()
+      .text()
+      .trim();
+    const originalPrice = priceAsnumber
+      .replace(/[^\d,\.]/g, "")
+      .replace(",", ".");
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() === "em estoque";
+
+    const productImages =
+      $("#landingImage").attr("data-a-dynamic-image") || "{}";
+    const imagesUrl = Object.keys(JSON.parse(productImages));
+
+    const priceSymbol = $(".a-price-symbol").first().text().trim();
+
+    const discountRate = $("span.savingsPercentage")
+      .text()
+      .trim()
+      .replace(/[-%]/g, "");
+
+    const data = {
+      url,
+      priceSymbol,
+      imagesUrl,
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+    };
+
+    console.log(data);
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
