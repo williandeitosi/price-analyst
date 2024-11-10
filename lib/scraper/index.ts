7655283597fc6abaa06de6f2d701203c1c8d3ee4
williandeitosi <williandeitosi@gmail.com>
2024-11-09 23:28:19 -0300

    feat: get product and add in MongoDB

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,17 +1,17 @@
+"use server";
+
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
-
-  // curl -i --proxy brd.superproxy.io:22225 --proxy-user
-  // brd-customer-hl_ffc6b9f3-zone-priceanalyst:uy88rifnm4pa -k "https://
-  // geo.brdtest.com/welcome.txt"
 
   const username = String(process.env.BRIGHT_DATA_USERNAME);
   const password = String(process.env.BRIGHT_DATA_PASSWORD);
   const port = 22225;
   const session_id = (1000000 * Math.random()) | 0;
+
   const options = {
     auth: {
       username: `${username}-session-${session_id}`,
@@ -21,57 +21,63 @@ export async function scrapeAmazonProduct(url: string) {
     port,
     rejectUnauthorized: false,
   };
+
   try {
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
-    const title = $("#productTitle").first().text().trim();
-    const currentPrice = $("span.a-price-whole")
-      .first()
-      .text()
-      .trim()
-      .replace(",", "");
-    const priceAsnumber = $(".a-price.a-text-price span.a-offscreen")
-      .first()
-      .text()
-      .trim();
-    const originalPrice = priceAsnumber
-      .replace(/[^\d,\.]/g, "")
-      .replace(",", ".");
+
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
 
     const outOfStock =
-      $("#availability span").text().trim().toLowerCase() === "em estoque";
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
 
-    const productImages =
-      $("#landingImage").attr("data-a-dynamic-image") || "{}";
-    const imagesUrl = Object.keys(JSON.parse(productImages));
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
 
-    const priceSymbol = $(".a-price-symbol").first().text().trim();
+    const imageUrls = Object.keys(JSON.parse(images));
 
-    const discountRate = $("span.savingsPercentage")
-      .text()
-      .trim()
-      .replace(/[-%]/g, "");
-    const lowestPrice = (Number(originalPrice) - Number(currentPrice)).toFixed(2)
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description = extractDescription($);
+
     const data = {
       url,
-      priceSymbol,
-      imagesUrl,
+      currency: currency || "$",
+      image: imageUrls[0],
       title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
       discountRate: Number(discountRate),
       category: "category",
       reviewsCount: 100,
       stars: 4.5,
       isOutOfStock: outOfStock,
-      highestPrice: Number(originalPrice),
-      lowestPrice: Number(lowestPrice)
-      // continue
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
     };
-
     console.log(data);
+    return data;
   } catch (error: any) {
-    throw new Error(`Failed to scrape product: ${error.message}`);
+    console.log(error);
   }
 }
